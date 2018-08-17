7b6b2e084da8314133d44c2c9b74d80d2adb5baa
HighGroundLover <kclarkoneil@yahoo.ca>
2018-08-16 17:36:05 -0700

    Cat Images initial commit

---
+++ Desktop/LighthouseLabsWeekThree/CatImages/CatImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CatImages
+//
+//  Created by Kit Clark-O'Neil on 2018-08-16.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Desktop/LighthouseLabsWeekThree/CatImages/CatImages/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CatImages
+//
+//  Created by Kit Clark-O'Neil on 2018-08-16.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
